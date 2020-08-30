133a21d96c1886d52ab12ecc3c97472943deb65f
cleopard99 <charles.leopard@outlook.com>
2020-08-30 20:39:23 +0100

    Add code

--- bot.js
+++ bot.js
@@ -18,12 +18,6 @@ function onAuthenticated(err, res) {
   } else if (res) console.log("Authentication successful. Running bot...\r\n");
 }
 
-// Listen to stream and tracks activity involving the bot
-const stream = T.stream("user");
-// When the bot is followed, call followed function
-stream.on("follow", followed);
-// When bot is tweeted at, call mentioned function
-stream.on("tweet", mentioned);
 
 // Get screenName of user who followed bot
 function followed(event) {
@@ -32,41 +26,41 @@ function followed(event) {
   // Post thankyou tweet to user
   T.post("statuses/update", { status: response }, tweeted);
 
-  console.log("I was followed by: @" + screenName);
+  console.log('I was followed by: @' + screenName);
 }
 
 function mentioned(tweet) {
-  console.log("Mention received! ", tweet);
+  console.log('Mention received! ', tweet)
   // Like mentions
-  T.post("favorites/create", { id: tweet.id }, logReponse);
+  T.post('favorites/create', { id: tweet.id }, logReponse)
   // Retweet mentions
   // T.post('statuses/retweet/:id', {id: tweet.id}, logReponse);
-  let screenName = tweet.user.screen_name;
-  let reply = "Hey @" + screenName + ", Thanks for the mentions!";
-  console.log(reply);
-  T.post("statuses/update", { status: reply }, tweeted);
+  let screenName = tweet.user.screen_name
+  let reply = 'Hey @' + screenName + ', Thanks for the mentions!'
+  console.log(reply)
+  T.post('statuses/update', { status: reply }, tweeted)
 }
 
 // logs errors from requests
 function logReponse(err) {
-  console.log(err);
+  console.log(err)
 }
 
 // Params to search for tweets according to query q
 let params = {
-  q: "#succulents", // Only required param
-  result_type: "popular",
+  q: '#succulents', // Only required param
+  result_type: 'recent',
   count: 10,
-  lang: "en",
+  lang: 'en',
 };
 
 function retweet() {
-  T.get("search/tweets", params, function (err, data) {
+  T.get('search/tweet', params, function (err, data) {
     if (!err) {
-      let retweetId = data.statuses.id;
-      T.post("statuses/retweet/" + retweetId, {}, checkRetweet);
+      let retweetId = data.statuses[0].id;
+      T.post('statuses/retweet/' + retweetId, {}, checkRetweet);
     } else {
-      console.log("There was an error during tweet search: ", error);
+      console.log('There was an error during tweet search: ', error);
     }
   });
 }
@@ -77,9 +71,16 @@ function checkRetweet(err, reply) {
         err +
           'Problem when retweeting. Possibly already retweeted this tweet!'
       )
-    : console.log("Retweeted: " + reply);
+    : console.log('Retweeted: ' + reply);
 }
 
+retweet
 // Retweet every 4 hours
-retweet();
 setInterval(retweet, 4 * 60 * 60 * 1000);
+
+// Listen to stream and tracks activity involving the bot
+var stream = T.stream('statuses/filter', { track: '@succulent_bot' });
+// When the bot is followed, call followed function
+stream.on("follow", followed);
+// When bot is tweeted at, call mentioned function
+stream.on("tweet", mentioned);
--- dist/bot.dev.js
+++ dist/bot.dev.js
@@ -15,14 +15,8 @@ function onAuthenticated(err, res) {
   if (err) {
     throw err;
   } else if (res) console.log("Authentication successful. Running bot...\r\n");
-} // Listen to stream and tracks activity involving the bot
+} // Get screenName of user who followed bot
 
-
-var stream = T.stream("user"); // When the bot is followed, call followed function
-
-stream.on("follow", followed); // When bot is tweeted at, call mentioned function
-
-stream.on("tweet", mentioned); // Get screenName of user who followed bot
 
 function followed(event) {
   var screenName = event.source.screen_name;
@@ -31,21 +25,21 @@ function followed(event) {
   T.post("statuses/update", {
     status: response
   }, tweeted);
-  console.log("I was followed by: @" + screenName);
+  console.log('I was followed by: @' + screenName);
 }
 
 function mentioned(tweet) {
-  console.log("Mention received! ", tweet); // Like mentions
+  console.log('Mention received! ', tweet); // Like mentions
 
-  T.post("favorites/create", {
+  T.post('favorites/create', {
     id: tweet.id
   }, logReponse); // Retweet mentions
   // T.post('statuses/retweet/:id', {id: tweet.id}, logReponse);
 
   var screenName = tweet.user.screen_name;
-  var reply = "Hey @" + screenName + ", Thanks for the mentions!";
+  var reply = 'Hey @' + screenName + ', Thanks for the mentions!';
   console.log(reply);
-  T.post("statuses/update", {
+  T.post('statuses/update', {
     status: reply
   }, tweeted);
 } // logs errors from requests
@@ -57,28 +51,36 @@ function logReponse(err) {
 
 
 var params = {
-  q: "#succulents",
+  q: '#succulents',
   // Only required param
-  result_type: "popular",
+  result_type: 'recent',
   count: 10,
-  lang: "en"
+  lang: 'en'
 };
 
 function retweet() {
-  T.get("search/tweets", params, function (err, data) {
+  T.get('search/tweet', params, function (err, data) {
     if (!err) {
-      var retweetId = data.statuses.id;
-      T.post("statuses/retweet/" + retweetId, {}, checkRetweet);
+      var retweetId = data.statuses[0].id;
+      T.post('statuses/retweet/' + retweetId, {}, checkRetweet);
     } else {
-      console.log("There was an error during tweet search: ", error);
+      console.log('There was an error during tweet search: ', error);
     }
   });
 }
 
 function checkRetweet(err, reply) {
-  err !== undefined ? console.log(err + 'Problem when retweeting. Possibly already retweeted this tweet!') : console.log("Retweeted: " + reply);
-} // Retweet every 4 hours
+  err !== undefined ? console.log(err + 'Problem when retweeting. Possibly already retweeted this tweet!') : console.log('Retweeted: ' + reply);
+}
 
+retweet; // Retweet every 4 hours
 
-retweet();
-setInterval(retweet, 4 * 60 * 60 * 1000);
+setInterval(retweet, 4 * 60 * 60 * 1000); // Listen to stream and tracks activity involving the bot
+
+var stream = T.stream('statuses/filter', {
+  track: '@succulent_bot'
+}); // When the bot is followed, call followed function
+
+stream.on("follow", followed); // When bot is tweeted at, call mentioned function
+
+stream.on("tweet", mentioned);
