ad91ee22fd8f340fb4ef228a4397da50b665d3b1
Charlie Leopard <charles.leopard@outlook.com>
2020-09-20 21:48:52 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebase = require("firebase");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnc-u034KY4YRAabgTJfoZrkCyzkL7YvM",
-    authDomain: "mytwitterbot-b13dc.firebaseapp.com",
-    databaseURL: "https://mytwitterbot-b13dc.firebaseio.com",
-    projectId: "mytwitterbot-b13dc",
-    storageBucket: "mytwitterbot-b13dc.appspot.com",
+    apiKey: "XXXXXXXX",
+    authDomain: "XXXXXXXX.firebaseapp.com",
+    databaseURL: "https://XXXXXXXX.firebaseio.com",
+    projectId: "XXXXXXXX",
+    storageBucket: "XXXXXXXX.appspot.com",
 };
 const app = firebase.initializeApp(firebaseConfig);
 module.exports =  db = app.database();
